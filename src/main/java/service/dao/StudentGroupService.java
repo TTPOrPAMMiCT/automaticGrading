912ef86962088b2eb3856ec3d4093ed49a1d4e70
TTPOrPAMMiCT <georgeusenko@list.ru>
2022-05-19 09:32:11 +0300

    create student group service

--- src/main/java/service/dao/StudentGroupService.java
+++ src/main/java/service/dao/StudentGroupService.java
@@ -1,4 +1,31 @@
 package service.dao;
 
+import dao.studentGroupDao.StudentGroupDao;
+import dao.studentGroupDao.StudentGroupDaoImpl;
+import entity.model.StudentGroup;
+import org.hibernate.Session;
+import utility.dataBase.HibernateUtil;
+
+import java.util.List;
+
 public class StudentGroupService {
+    StudentGroupDao studentGroupDao = new StudentGroupDaoImpl();
+    public List<StudentGroup> findAll() {
+        Session session = HibernateUtil.openSession();
+        List<StudentGroup> studentGroups = studentGroupDao.findAll(session);
+        HibernateUtil.closeSession(session);
+        return studentGroups;
+    }
+
+    public void create(StudentGroup studentGroup ) {
+        Session session = HibernateUtil.openSessionWithTransaction();
+        studentGroupDao.createGroup(session, studentGroup);
+        HibernateUtil.closeSessionWithTransaction(session);
+    }
+
+    public void delete(StudentGroup studentGroup) {
+        Session session = HibernateUtil.openSessionWithTransaction();
+        studentGroupDao.deleteGroup(session, studentGroup);
+        HibernateUtil.closeSessionWithTransaction(session);
+    }
 }
